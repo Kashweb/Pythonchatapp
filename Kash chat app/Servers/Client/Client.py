f6d0dc199cf8696486082e0c5c89169a19b945a3
Kashish Chawla <69769135+kashishchawla1@users.noreply.github.com>
2022-01-20 23:41:19 +0930

    Update Client.py
    adding the constants and variables

--- Kash chat app/Servers/Client/Client.py
+++ Kash chat app/Servers/Client/Client.py
@@ -1,3 +1,19 @@
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread, Lock
 import time
+
+#Gobal constants
+HOST = ''
+PORT = 5001
+BUFSIZ = 500
+MAX_CONNETIONS = 20
+ADDR = (HOST, PORT)
+Persons = []
+
+#Global Variables
+SERVER = socket(AF_INET, SOCK_STREAM)
+SERVER.bind(ADDR)  # set up server
+
+
+class client:
+    def __init__(self, name):
